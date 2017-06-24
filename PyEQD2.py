a561a314574d3997177bf75f4008f93cf57368a5
chris201706 <christopher.schuppert@gmail.com>
2017-06-24 16:49:16 +0200

    Add files via upload

---
+++ PyEQD2.py
@@ -0,0 +1,67 @@
+'''
+Version:    1.0
+Status:     finished
+Date:       Jun 22, 2017
+Author:     ***REMOVED***
+Summary:    Import/Export DICOM RT-DOSE, convert to EQD2, plot dose distribution and legend. Object-Oriented.
+'''
+
+
+################
+### Preamble ###
+################
+
+from corefunctions import *
+from plotting import *
+
+
+########################
+### Class Definition ###
+########################
+
+class RTDose:
+    def __init__(self, filepath):
+        self.dcmfile = import_dcm(filepath)
+        self.name, self.ext = os.path.splitext(os.path.basename(filepath))
+        if is_modality(self.dcmfile,'RTDOSE'):
+            self.pixelarray, self.shape = read_dose(self.dcmfile)
+            self.pixelspacing = self.dcmfile.PixelSpacing
+            self.raw_maxval = np.amax(self.pixelarray)
+            self.scalefactor = self.dcmfile.DoseGridScaling
+            self.precision = self.dcmfile.BitsAllocated
+            self.signedness = self.dcmfile.PixelRepresentation # Cf. [A] for PixelRepresentation: 0 is unsigned, 1 is signed
+            print("––New instance of Class RTDose has been initiated.")
+        else:
+            raise ValueError("Input file is not of modality 'RTDOSE'.(Returning False.)")
+    def make_EQD2(self, fn, abratio):
+        self.pixelarray, self.scalefactor = dcm_to_eqd2(self.pixelarray, self.scalefactor, self.precision, self.signedness, self.raw_maxval, fn, abratio)
+    def multiply(self, factor):
+        self.pixelarray, self.scalefactor = dcm_multiply(self.pixelarray, self.scalefactor, self.precision, self.signedness, self.raw_maxval, factor)
+    def plot(self):
+        rtdose_plot(self.pixelarray, self.pixelspacing, self.name, newjet)
+    def plot_legend(self):
+        legend(newjet)
+        pass
+    def export(self):
+        export_dcm(self.dcmfile, self.name)   # simply exports to current directory (where plots and legends are also saved – consistency!)
+
+''' Test
+dicompath = '/Users/macuser/Downloads/testfile.dcm'
+a = RTDose(dicompath)
+a.name
+a.plot_legend()
+a.plot()
+a.make_EQD2(8,3)
+a.multiply(8)
+a.make_EQD2(1,3)
+a.export()
+\Test '''
+
+
+##################
+### References ###
+##################
+# [A] http://dicomiseasy.blogspot.de/2012/08/chapter-12-pixel-data.html
+
+
+
